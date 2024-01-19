aaa45eec49ab9d02018fe2ab055dc5aca358825e
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2024-01-19 13:42:10 -0600

    connect to neon db + venue model

--- whats_happening/main_app/models.py
+++ whats_happening/main_app/models.py
@@ -1,4 +1,4 @@
-# from django.db import models
+from django.db import models
 
 # # Create your models here.
 # #
@@ -20,11 +20,11 @@
     
     
     
-# class Venue(models.Model):
-#     name = models.CharField(max_length=100)
-#     location = models.CharField(max_length=100)
-#     description = models.TextField(max_length=250)
-#     # image = models.ImageField(upload_to='images/', blank=True)
+class Venue(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    # image = models.ImageField(upload_to='images/', blank=True)
     
 #     def __str__(self):
 #         return self.name
