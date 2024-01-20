a220ab630e0a225d85a4407b2e5c59805d9d56ce
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2024-01-20 10:18:16 -0600

    filter my planned events to show or hide past events

--- whats_happening/main_app/urls.py
+++ whats_happening/main_app/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     # path('about/', views.about, name='about')
     path('events/', views.EventList.as_view(), name='index'),
-    path('events/myowned', views.MyOwnedEventList.as_view(), name='myownedindex'),
+    path('events/myowned/', views.MyOwnedEventList.as_view(), name='myownedindex'),
+    path('events/myowned/include_past', views.MyOwnedWithPastEventList.as_view(), name='myownedindex_withpast'),
     # path('events/mine', views.MyEventList.as_view(), name='myindex'),
 
     path('accounts/signup/', views.signup, name='signup'),
--- whats_happening/main_app/views.py
+++ whats_happening/main_app/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -16,15 +18,28 @@ class EventList(ListView):
   model = Event
   template_name = 'events/index.html'
   
+  def get_queryset(self):
+    return Event.objects.filter(date__gte=datetime.date.today())
+  
 class MyOwnedEventList(EventList):
   def get_queryset(self):
-    print(f"self.request.user = {self.request.user}")
-    return Event.objects.filter(owner=self.request.user)
+    return Event.objects.filter(owner=self.request.user, date__gte=datetime.date.today())
 
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
     context['page_title'] = 'My Owned Events'
+    context['include_past'] = False
     return context
+  
+class MyOwnedWithPastEventList(MyOwnedEventList):
+  def get_queryset(self):
+    return Event.objects.filter(owner=self.request.user)
+  
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['include_past'] = True
+    return context
+
 
 def signup(request):
   error_message = ''
